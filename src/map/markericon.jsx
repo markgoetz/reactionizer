18284c160f9d3e02f22ad80121d5046e2613f61c
Mark Goetz <mark@markandrewgoetz.com>
2016-10-30 11:17:02 -0500

    Fixed svg size on map; fixed an error message in the console.

--- src/map/markericon.jsx
+++ src/map/markericon.jsx
@@ -5,12 +5,12 @@ var Marker = require("react-google-maps").Marker;
 var MarkerIcon = React.createClass({
 	propTypes: {
 		team: React.PropTypes.instanceOf(Team).isRequired,
-		mapHolderRef: React.PropTypes.objects
+		mapHolderRef: React.PropTypes.object
 	},
 	render: function() {
 		return <Marker
 			position={ { lat: this.props.team.lat, lng: this.props.team.lon } }
-			icon={ {url: this.props.team.getLogoURL(), anchor: {x:10, y:10}} }
+			icon={ {url: this.props.team.getLogoURL(), anchor: {x:15, y:15}, size: { height: 30, width: 30 }} }
 			title={this.props.team.name}
 			zIndex={2}
 			mapHolderRef={this.props.mapHolderRef}

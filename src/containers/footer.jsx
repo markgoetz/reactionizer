0fe2db8121f5d7c8155037e71158bf1c5743c7a1
Mark Goetz <mark@markandrewgoetz.com>
2016-12-12 23:21:13 -0600

    Changed header and footer to functional components

--- src/containers/footer.jsx
+++ src/containers/footer.jsx
@@ -1,11 +1,9 @@
 var React = require("react");
 require("./_footer.scss");
 
-var Footer = React.createClass({
-	render: function() {
-		var year = new Date().getYear() + 1900;
-		return <footer>Divisionizer is copyright &copy; {year} <a href="http://www.markandrewgoetz.com/" target="_blank">Mark Goetz</a></footer>;
-	}
-});	
+var Footer = function() {
+	var year = new Date().getYear() + 1900;
+	return <footer>Divisionizer is copyright &copy; {year} <a href="http://www.markandrewgoetz.com/" target="_blank">Mark Goetz</a></footer>;
+};
 
 module.exports = Footer;
--- src/containers/header.jsx
+++ src/containers/header.jsx
@@ -1,12 +1,10 @@
 var React = require("react");
 require("./_header.scss");
 
-var Header = React.createClass({
-	render: function() {
-		return <header>
-			<img src="/images/logo.png" width="176" height="40" />
-		</header>;
-	}
-});	
+var Header = function() {
+	return <header>
+		<img src="/images/logo.png" width="176" height="40" />
+	</header>;
+};
 
 module.exports = Header;

2b92e41c54497c9c53b752cf38a6e63a77bd118d
Mark Goetz <breakawayblue@gmail.com>
2018-06-09 16:29:57 -0500

    Linting fixes for webpack config

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -1,48 +1,46 @@
-var PATHS = {
-	src: "./src/",
-	dist: "./dist/",
-	dist_js: "./dist/js/",
-	dist_css: "./dist/css/",
-	exclude: [/node_modules/, /\.spec\.js/]
+import ExtractTextPlugin from 'extract-text-webpack-plugin';
+
+const PATHS = {
+  src: './src/',
+  dist: './dist/',
+  exclude: [/node_modules/, /\.spec\.js/],
 };
 
-var ExtractTextPlugin = require("extract-text-webpack-plugin");
-
 module.exports = {
-	entry: PATHS.src + "entry.jsx",
-	output: {
-		path: PATHS.dist,
-		filename: "js/reactionizer.js"
-	},
-	module: {
-		loaders: [{
-			test: /\.jsx?$/,
-			exclude: PATHS.exclude,
-			loaders: ["babel"]
-		},
-		{
-			test: /\.scss$/,
-			exclude: PATHS.exclude,
-			loader: ExtractTextPlugin.extract("css!sass")
-		},
-		{
-			test: /\.json$/,
-			exclude: PATHS.exclude,
-			loader: "json"
-		},
-		{
-			test: /\.svg$/,
-			loader: "svg-sprite?name=logo-[name]"
-		}]
-	},
-	devtool: "source-map",
-	plugins: [
-		new ExtractTextPlugin("css/reactionizer.css")
-	],
-	resolve: {
-		extensions: ["", ".js", ".jsx"]
-	},
-	devServer: {
-		contentBase: "./dist/"
-	}
+  entry: `${PATHS.src}entry.jsx`,
+  output: {
+    path: PATHS.dist,
+    filename: 'js/reactionizer.js',
+  },
+  module: {
+    loaders: [{
+      test: /\.jsx?$/,
+      exclude: PATHS.exclude,
+      loaders: ['babel'],
+    },
+    {
+      test: /\.scss$/,
+      exclude: PATHS.exclude,
+      loader: ExtractTextPlugin.extract('css!sass'),
+    },
+    {
+      test: /\.json$/,
+      exclude: PATHS.exclude,
+      loader: 'json',
+    },
+    {
+      test: /\.svg$/,
+      loader: 'svg-sprite?name=logo-[name]',
+    }],
+  },
+  devtool: 'source-map',
+  plugins: [
+    new ExtractTextPlugin('css/reactionizer.css'),
+  ],
+  resolve: {
+    extensions: ['', '.js', '.jsx'],
+  },
+  devServer: {
+    contentBase: './dist/',
+  },
 };
--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -1,64 +1,62 @@
-var PATHS = {
-	src: "./src/",
-	dist: "./dist/",
-	dist_js: "./dist/js/",
-	dist_css: "./dist/css/",
-	exclude: [/node_modules/, /\.spec\.js/]
+const PATHS = {
+  src: './src/',
+  dist: './dist/',
+  exclude: [/node_modules/, /\.spec\.js/],
 };
 
-var ExtractTextPlugin = require("extract-text-webpack-plugin");
-var webpack = require("webpack");
+let ExtractTextPlugin = require('extract-text-webpack-plugin');
+let webpack = require('webpack');
 
 module.exports = {
-	devtools: "",
-	entry: PATHS.src + "entry.jsx",
-	output: {
-		path: PATHS.dist,
-		filename: "js/reactionizer.js"
-	},
-	module: {
-		loaders: [{
-			test: /\.jsx?$/,
-			exclude: PATHS.exclude,
-			loader: "babel"
-		},
-		{
-			test: /\.scss$/,
-			exclude: PATHS.exclude,
-			loader: ExtractTextPlugin.extract("css!sass")
-		},
-		{
-			test: /\.json$/,
-			exlude: PATHS.exclude,
-			loader: "json"
-		},
-		{
-			test: /\.svg$/,
-			loaders: [
-				"svg-sprite?name=logo-[name]",
-				"svgo-loader?config=svgoConfig1"
-			]
-		}]
-	},
-	plugins: [
-		new ExtractTextPlugin("css/reactionizer.css"),
-		new webpack.DefinePlugin({
-			"process.env": {
-				NODE_ENV: JSON.stringify("production")
-			}
-		}),
-		new webpack.optimize.UglifyJsPlugin({
-			mangle: {
-				except: ["module", "exports", "require"]
-			}
-		})
-	],
-	resolve: {
-		extensions: ["", ".js", ".jsx"]
-	},
-	svgoConfig1: {
-		plugins:[
-			{removeTitle:true}
-		]
-	}
+  devtools: '',
+  entry: `${PATHS.src}entry.jsx`,
+  output: {
+    path: PATHS.dist,
+    filename: 'js/reactionizer.js',
+  },
+  module: {
+    loaders: [{
+      test: /\.jsx?$/,
+      exclude: PATHS.exclude,
+      loader: 'babel',
+    },
+    {
+      test: /\.scss$/,
+      exclude: PATHS.exclude,
+      loader: ExtractTextPlugin.extract('css!sass'),
+    },
+    {
+      test: /\.json$/,
+      exlude: PATHS.exclude,
+      loader: 'json',
+    },
+    {
+      test: /\.svg$/,
+      loaders: [
+        'svg-sprite?name=logo-[name]',
+        'svgo-loader?config=svgoConfig1',
+      ],
+    }],
+  },
+  plugins: [
+    new ExtractTextPlugin('css/reactionizer.css'),
+    new webpack.DefinePlugin({
+      'process.env': {
+        NODE_ENV: JSON.stringify('production'),
+      },
+    }),
+    new webpack.optimize.UglifyJsPlugin({
+      mangle: {
+        except: ['module', 'exports', 'require'],
+      },
+    }),
+  ],
+  resolve: {
+    extensions: ['', '.js', '.jsx'],
+  },
+  svgoConfig1: {
+    plugins: [
+      { removeTitle: true },
+    ],
+  },
 };
